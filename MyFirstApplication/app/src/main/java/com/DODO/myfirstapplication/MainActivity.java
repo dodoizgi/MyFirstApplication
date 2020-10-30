431fcd3a549fef4dcdf62f94e051824c6d8af575
dodoizgi <45079943+dodoizgi@users.noreply.github.com>
2020-10-30 20:33:51 +0300

    MyFirstApplication
    MyFirstApplication

---
+++ MyFirstApplication/app/src/main/java/com/DODO/myfirstapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.DODO.myfirstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeImage(View view){
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView5);
+        Button button = findViewById(R.id.button20);
+
+        imageView.setImageResource(R.drawable.asas);
+    }
+
+
+}
